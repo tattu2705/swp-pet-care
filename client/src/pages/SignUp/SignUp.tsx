f3f1f5d61c421446e9b3c7dd44d3dcd5421a6e97
tattu2705 <tattupro2705@gmail.com>
2024-07-11 21:35:24 +0700

    Code admin

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -18,7 +18,7 @@ function Copyright(props: any) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Pet Care
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
